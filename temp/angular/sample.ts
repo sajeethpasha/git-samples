5c3f5a86cd7118c3c22e7b16c596788280e10780
sajeethpasha <sajeethpasha@gmail.com>
2024-11-11 22:17:52 +0530

    qs70bzRvQZ - 2024-11-11 22:17:52

--- temp/angular/sample.ts
+++ temp/angular/sample.ts
@@ -1,15 +1,28 @@
-filteredSubTenantList: any[] = [
-    {
-      label: 'All',
-      value: this.SUB_TENANT_ID_FOR_ALL
-    },
-    {
-      label: 'Tenant 1',
-      value: 'tenant1'
-    },
-    {
-      label: 'Tenant 2',
-      value: 'tenant2'
-    }
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaign-details',
+  templateUrl: './campaign-details.component.html',
+  styleUrls: ['./campaign-details.component.scss']
+})
+export class CampaignDetailsComponent {
+  campaigns = [
+    { name: 'Campaign-Name', link: '/campaign/1' },
+    { name: 'Campaign-Name-with-extra-length', link: '/campaign/2' },
+    { name: 'Campaign-Name-with-extra-length', link: '/campaign/3' },
+    { name: 'Campaign-Name-with-extra-length', link: '/campaign/4' },
   ];
   
+  isDropdownOpen = false;
+
+  get displayCampaignText() {
+    if (this.campaigns.length > 1) {
+      return `${this.campaigns[0].name} (+${this.campaigns.length - 1})`;
+    }
+    return this.campaigns[0]?.name || 'No Campaigns';
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+}
