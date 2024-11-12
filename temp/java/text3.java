81e5904470f3c590fba013a8c45f129a7ea810d4
sajeethpasha <sajeethpasha@gmail.com>
2024-11-12 18:45:57 +0530

    XWFfeoWufG - 2024-11-12 18:45:56

--- temp/java/text3.java
+++ temp/java/text3.java
@@ -1,42 +1,52 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public Mono<WorkOrder[]> getGroupNumberDetails(String organizationCode, String workOrderGroupNumber) {
+    return Mono.deferContextual(c -> {
+        long startTime = System.currentTimeMillis();
 
-public class text3 {
-    private static final Logger logger = LoggerFactory.getLogger(text3.class);
+        return c.getOrEmpty("AzureToken")
+                .map(azureToken -> client.get()
+                        .uri(uriBuilder -> {
+                            buildListWorkOrdersGroupNumberUri(organizationCode, workOrderGroupNumber, uriBuilder);
+                            return uriBuilder.build();
+                        })
+                        .header("Authorization", c.get("AzureToken").toString())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .retrieve()
+                        .onStatus(status -> status.is4xxClientError() || status.is5xxServerError(), response -> {
+                            logger.error("Received an error status code: {}", response.statusCode());
+                            return response.bodyToMono(ApiError.class)
+                                    .flatMap(error -> Mono.error(new WorkOrderDomainException(error)));
+                        })
+                        .bodyToMono(new ParameterizedTypeReference<PagedResponse<WorkOrder>>() {})
+                        .map(workOrdersResponse -> {
+                            long endTime = System.currentTimeMillis();
+                            logger.info("Time taken for first API call: {} ms", (endTime - startTime));
+                            return Arrays.stream(workOrdersResponse.getItems()).toArray(WorkOrder[]::new);
+                        })
+                )
+                .orElseGet(() -> {
+                    long startTimeSecondCall = System.currentTimeMillis();
 
-    public Mono<InventoryLotNumber> createLotNumber(String organizationCode, String workOrderNumber, Boolean partialLotFlag) {
-        long startTime = System.nanoTime();  // Start time in nanoseconds
-
-        return client.get()
-            .uri(uriBuilder -> {
-                uriBuilder.path("/code2Logic")
-                    .queryParam("organizationCode", organizationCode)
-                    .queryParam("workOrderNumber", workOrderNumber)
-                    .queryParam("partialLotFlag", partialLotFlag);
-                return uriBuilder.build();
-            })
-            .accept(MediaType.APPLICATION_JSON)
-            .retrieve()
-            .onStatus(
-                status -> status.is4xxClientError() || status.is5xxServerError(),
-                response -> response.bodyToMono(String.class)
-                    .flatMap(error -> Mono.error(new RuntimeException(error)))
-            )
-            .bodyToMono(new ParameterizedTypeReference<InventoryLotNumberResponse[]>() {})
-            .map(response -> {
-                if (Arrays.stream(response).count() > 0) {
-                    return response[0].toModel();
-                }
-                throw new RuntimeException("No response returned from lot number service");
-            })
-            .onErrorMap(error -> {
-                logger.error("Non-Group lot number creation failed with exception: {}", error.getMessage());
-                return new LotNumberException(error.getMessage());
-            })
-            .doOnSuccess(response -> {
-                long endTime = System.nanoTime();  // End time in nanoseconds
-                long durationInMillis = (endTime - startTime) / 1_000_000;  // Convert nanoseconds to milliseconds
-                logger.info("API call elapsed time: {} ms", durationInMillis);
-            });
-    }
+                    return client.mutate()
+                            .baseUrl(workOrderInternalUrl)
+                            .defaultHeader("X-API-KEY", workOrderDomainApiKey)
+                            .build().get()
+                            .uri(uriBuilder -> {
+                                buildListWorkOrdersGroupNumberUri(organizationCode, workOrderGroupNumber, uriBuilder);
+                                return uriBuilder.build();
+                            })
+                            .accept(MediaType.APPLICATION_JSON)
+                            .retrieve()
+                            .onStatus(status -> status.is4xxClientError() || status.is5xxServerError(), response -> {
+                                logger.error("Received an error status code for fallback: {}", response.statusCode());
+                                return response.bodyToMono(ApiError.class)
+                                        .flatMap(error -> Mono.error(new WorkOrderDomainException(error)));
+                            })
+                            .bodyToMono(new ParameterizedTypeReference<PagedResponse<WorkOrder>>() {})
+                            .map(workOrdersResponse -> {
+                                long endTimeSecondCall = System.currentTimeMillis();
+                                logger.info("Time taken for second API call: {} ms", (endTimeSecondCall - startTimeSecondCall));
+                                return Arrays.stream(workOrdersResponse.getItems()).toArray(WorkOrder[]::new);
+                            });
+                });
+    });
 }
